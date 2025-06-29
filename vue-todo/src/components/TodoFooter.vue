f17a2f6b58fd3bce241af6df8386c5d269299297
oh euna <erropp89@naver.com>
2025-06-29 17:36:34 +0900

    feat: Footer에 전체 리스트 삭제 기능 구현

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
-    footer
+  <div class="clearAllContainer">
+    <span class= "clearAllBtn" v-on:click="clearTodo">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
 
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    }
+  }
+
 }
 </script>
 
-<style>
+<style scoped>
+.clearAllContainer {
+  background-color: white;
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  margin: 0 auto;
+}
+
+.clearAllBtn {
+  color: #62acde;
+  cursor: pointer;
+  font-weight: bold;
+}
 
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -49,7 +49,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 .checkBtn{
   color: #62acde;
